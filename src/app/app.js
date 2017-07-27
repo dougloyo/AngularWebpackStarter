9d89bcda63e8e57e0d3fcde0cce1b545dd96b788
Douglas Loyo <douglasloyo@gmail.com>
2017-07-27 08:26:02 -0500

    Updating libs and routing to use HTML5 clean routes.

--- src/app/app.js
+++ src/app/app.js
@@ -8,13 +8,16 @@ angular.module("app", [
     "app.login",
     "app.home"
 ])
-    .config(['$routeProvider', function($routeProvider) {
-    $routeProvider
-        .when('/',
-        {
-            controller: 'loginController',
-            controllerAs: 'vm',
-            template: require('./modules/login/login.view.html')
-        })
-        .otherwise({ redirectTo: '/' });
+.config(['$routeProvider','$locationProvider', function($routeProvider,$locationProvider) {
+$routeProvider
+    .when('/',
+    {
+        controller: 'loginController',
+        controllerAs: 'vm',
+        template: require('./modules/login/login.view.html')
+    })
+    .otherwise({ redirectTo: '/' });
+
+    // use the HTML5 History API
+    $locationProvider.html5Mode(true);
 }]); 
