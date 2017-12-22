bacaf08ff7dbc8071bd033b837dc4536348cd1ba
Douglas Loyo <douglasloyo@gmail.com>
2017-12-21 18:10:56 -0600

    Moving Chart to a nested directive.

---
+++ src/app/modules/shared/directives/charts/chartDirective.js
@@ -0,0 +1,21 @@
+﻿angular.module("app.directives")
+    .directive('chartDirective', [function () {
+        return {
+            restrict: 'E',
+            template: require("./chart.view.html"),
+            controllerAs:'ctrl',
+            scope:{
+                data:'='
+            },
+            controller: ['$scope', function ($scope) {
+                var ctrl = this;
+                ctrl.model = {};
+                ctrl.model.x = "here";
+
+                ctrl.chart=$scope.data;
+
+                //console.log($scope.data);
+            }],
+            
+        };
+    }]);
