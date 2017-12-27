208c6771aaabbaf72b796a5f52b910a037d77428
Douglas Loyo <douglasloyo@gmail.com>
2017-12-27 15:51:32 -0600

    Adding chart data changes on click

--- src/app/modules/home/home.module.js
+++ src/app/modules/home/home.module.js
@@ -17,8 +17,8 @@ angular.module("app.home", [])
 
         ctrl.chart={ 
             labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
-            series: ['Series A', 'Series B'],
-            data: [ [65, 59, 80, 81, 56, 55, 40], [28, 48, 40, 19, 86, 27, 90] ],
+            series: ['Series A'], //['Series A', 'Series B'],
+            data: [ [65, 59, 80, 81, 56, 55, 40] ], //, [28, 48, 40, 19, 86, 27, 90] ],
             options:{
                 responsive: true,
                 maintainAspectRatio: false
--- src/app/modules/shared/directives/charts/chartDirective.js
+++ src/app/modules/shared/directives/charts/chartDirective.js
@@ -14,6 +14,24 @@
 
                 ctrl.chart=$scope.data;
 
+                ctrl.onChartClick = function (points, evt) {
+                    
+                    var dataPoints = getRandomInt(5,15);
+                    var newData = [];
+                    for(var i=0; i<dataPoints; i++)
+                        newData.push(getRandomInt(5,100));
+                    
+                    $scope.$apply(function () {
+                        ctrl.chart.data = newData;
+                    });
+                    
+                    console.log(newData);
+                };
+
+                function getRandomInt(min, max) {
+                    return Math.floor(Math.random() * (max - min + 1)) + min;
+                }
+
                 //console.log($scope.data);
             }],
             
